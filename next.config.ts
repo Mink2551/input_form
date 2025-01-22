d7391e718bb6e8014fe335ef4db6112655dc06bd
pacharapol pimpa <thefirestong@gmail.com>
2025-01-22 09:52:54 +0700

    test

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,  // ปิดการตรวจสอบ ESLint ในระหว่างการ build
-  },
-  // เพิ่มการตั้งค่าอื่นๆ ที่คุณต้องการ
+  /* config options here */
 };
 
 export default nextConfig;
